978058d7efc39c24833f1954e269521af2b63211
Mubarak Agboola <agboolalucas2000@gmail.com>
2023-04-28 12:59:34 +0100

    start command

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ thesymptoms = {}
 
 def start(update, context):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id,
-                             text="I'm illPredbot and I diagnose illnesses based on provided symptoms.")
+    context.bot.send_message(chat_id=chat_id, text="I'm illPredbot and I diagnose illnesses based on provided symptoms.")
     context.bot.send_message(chat_id=chat_id, text="click button below keyboard to enter symptoms",
                              reply_markup=symptom_keyboard())
     return FIRST_STEP
@@ -199,16 +198,22 @@ def main():
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(conv_handler)
+    updater.start_polling()
 
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(os.environ.get('PORT', 5000)),
-                          url_path=telegram_bot_token,
-                          webhook_url="https://illness-detector.onrender.com/" + telegram_bot_token
-                          )
-    updater.bot.setWebhook('https://illness-detector.onrender.com/' + telegram_bot_token)
-
-    updater.idle()
-
+# def main():
+#     updater = Updater(token=telegram_bot_token, use_context=True)
+#     dispatcher = updater.dispatcher
+#
+#     dispatcher.add_handler(conv_handler)
+#
+#     updater.start_webhook(listen="0.0.0.0",
+#                           port=int(os.environ.get('PORT', 5000)),
+#                           url_path=telegram_bot_token,
+#                           webhook_url="https://illness-detector.onrender.com/" + telegram_bot_token
+#                           )
+#     updater.bot.setWebhook('https://illness-detector.onrender.com/' + telegram_bot_token)
+#
+#     updater.idle()
 
 if __name__ == "__main__":
     main()
